a6e30a6efb62d8bfcbfd1b757e330ea903d7f3d8
Adam Ryan <thestormguy33@gmail.com>
2023-10-27 08:48:11 +0100

    adams files
    adams files

---
+++ deck.py
@@ -0,0 +1,11 @@
+class Deck():
+    def __init__(self):
+        self.cards = []
+
+    def add_cards(self, cards):
+        self.cards.extend(cards)
+
+    def create_cards(self):
+        card = []
+
+    
